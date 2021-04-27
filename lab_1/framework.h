641598163a59382c1ac76d96de47648c4a9e74ce
Anastasiya5530 <79335470+Anastasiya5530@users.noreply.github.com>
2021-04-27 16:57:11 +0300

    Add files via upload

---
+++ lab_1/framework.h
@@ -0,0 +1,5 @@
+﻿#pragma once
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // Некоторые конструкторы CString будут явными.
+
+#include <atlbase.h>
+#include <atlstr.h>
---
+++ lab_1/func.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <stdio.h>
+#include <math.h>
+
+extern double x, y, result_task7;
+void function_task7();
